197c6b29dae1dbdfbd1c977b5e1a4ce6cc115663
Archit130 <architdeora1999@gmail.com>
2023-07-30 22:15:12 +0530

    up

--- js/index.js
+++ js/index.js
@@ -15,3 +15,9 @@ window.addEventListener("scroll", function () {
 });
 
 console.log("hello world");
+
+const icon = document.querySelector(".icon");
+const search = document.querySelector(".search");
+icon.onclick = function () {
+  search.classList.toggle("active");
+};
