cc660a08ca990e511d4409e510e912ad49c2de51
Archit130 <architdeora1999@gmail.com>
2023-08-03 10:59:03 +0530

    JO

--- js/index.js
+++ js/index.js
@@ -1,19 +1,3 @@
-// Custom JavaScript for scroll up behavior
-let prevScrollPos = window.pageYOffset;
-const navbar = document.querySelector(".navbar");
-
-window.addEventListener("scroll", function () {
-  const currentScrollPos = window.pageYOffset;
-  if (prevScrollPos > currentScrollPos) {
-    // Scrolling up
-    navbar.classList.remove("scrolled-up");
-  } else {
-    // Scrolling down
-    navbar.classList.add("scrolled-up");
-  }
-  prevScrollPos = currentScrollPos;
-});
-
 console.log("hello world");
 
 const icon = document.querySelector(".icon");
